12cf420727311a65c4392238b03776408830568c
John Alexis Guerra Gómez <john.guerra@gmail.com>
2024-05-24 16:29:06 -0700

    Improving the aesthetics of the timbox options

--- src/BrushContextMenu.js
+++ src/BrushContextMenu.js
@@ -14,61 +14,79 @@ function BrushContextMenu({ target, callback }) {
 
   let _brush;
 
-  let contextMenu = htl.html
-  `<div class="__ts_contextMenu" style="display: none; z-index: 2; position: absolute" >
-      <ul>
-        <li>Mode</li>
-        ${intersectE}
-        <label for="__ts_c_intersect">Intersect</label>
-        ${containsE}
-        <label for="__ts_c_contains">Contains</label>
-        <li>Aggregation</li>
-        ${andE}
-        <label for="__ts_c_and">And</label>
-        ${orE}
-        <label for="__ts_c_or">Or</label>
-      </ul>
+  let contextMenu = htl.html`<div class="__ts_contextMenu" style="display: none; z-index: 2; position: absolute" >
+      
+      
+        <div class="__ts_option_label"><strong>Mode</strong></div>
+        <div class="__ts_option_values">
+          <div>
+            ${intersectE}
+            <label for="__ts_c_intersect">Intersect</label>
+          </div>
+          <div>
+            ${containsE}
+            <label for="__ts_c_contains">Contains</label>
+          </div>
+        </div>
+      
+
+      
+        <div class="__ts_option_label"><strong>Aggregation</strong></div>
+        <div class="__ts_option_values">
+          <div>
+            ${andE}
+            <label for="__ts_c_and">And</label>
+          </div>
+          <div>
+            ${orE}
+            <label for="__ts_c_or">Or</label>
+          </div>
+        </div>
+      
+      
 
       <style> 
         .__ts_contextMenu { 
+            border-radius: 3px;
+            padding: 4px;
             position: absolute; 
-            text-align: center; 
-            background: lightgray; 
-            border: 1px solid black; 
+            width: max-content;
+            background: #f6f6f6;
+            opacity: 0.9;
+            border: 1px solid #ccc; 
+            font-family: sans-serif;
+            font-size: 0.8rem;
+            grid-template-columns: 1fr auto;
+            grid-row-gap: 7px;
+            box-shadow: 2px 2px 1px 0px #888888;
+        }         
+
+        .__ts_contextMenu  .__ts_option_values:hover { 
+            background: #eee; 
         } 
-      
-        .__ts_contextMenu ul { 
-            padding: 0px; 
-            margin: 0px; 
-            min-width: 150px; 
-            list-style: none; 
-        } 
-      
-        .__ts_contextMenu ul li { 
-            padding-bottom: 7px; 
-            padding-top: 7px; 
-            border: 1px solid black; 
-        } 
-      
-      
-        .__ts_contextMenu ul li:hover { 
-            background: darkgray; 
-        } 
+
+        .__ts_contextMenu input[type="radio"] {
+          margin-top: -1px;
+          vertical-align: middle;
+        }
       </style> 
-      </div>`;
+    </div>`;
 
   target.appendChild(contextMenu);
 
   contextMenu.onmouseleave = () => contextMenu.__hide();
 
   function onChange() {
-    let brushMode = intersectE.checked ? BrushModes.Intersect : BrushModes.Contains;
-    let brushAggregation = andE.checked ? BrushAggregation.And : BrushAggregation.Or;
+    let brushMode = intersectE.checked
+      ? BrushModes.Intersect
+      : BrushModes.Contains;
+    let brushAggregation = andE.checked
+      ? BrushAggregation.And
+      : BrushAggregation.Or;
     callback(brushMode, brushAggregation, _brush);
   }
 
-
-  contextMenu.__hide = () => contextMenu.style.display = "none";
+  contextMenu.__hide = () => (contextMenu.style.display = "none");
   contextMenu.__show = (mode, aggregation, pxX, pxY, brush) => {
     _brush = brush;
     switch (mode) {
@@ -80,7 +98,9 @@ function BrushContextMenu({ target, callback }) {
         break;
       default:
         intersectE.checked = true;
-        log("🚫 ERROR The method elected to compute the selection are not support");
+        log(
+          "🚫 ERROR The method elected to compute the selection are not support"
+        );
     }
 
     switch (aggregation) {
@@ -95,14 +115,12 @@ function BrushContextMenu({ target, callback }) {
         log("🚫 ERROR The aggregation method elected are not support");
     }
 
-    contextMenu.style.display = "block";
+    contextMenu.style.display = "grid";
     contextMenu.style.left = pxX + "px";
     contextMenu.style.top = pxY + "px";
   };
 
-
   return contextMenu;
 }
-
 
 export default BrushContextMenu;
